a996107be17a37bbfecdcd4846d8ea02953c2b38
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-05-16 21:43:51 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        namb: 1,
+        question: "What does HTML stand for?",
+        answer: "A. google",
+        options: [
+            "A. google",
+            "B. boostrap",
+            "C. instagram",
+            "D. mozila"
+        ]
+    },
+    {
+        namb: 2,
+        question: "Can I park here?",
+        answer: "B. It's the same place.",
+        options: [
+            "A. Only for half an hour",
+            "B. It's the same place.",
+            "C. It's the same.",
+            "D. same place.",
+        ]
+    },
+    {
+        namb: 3,
+        question: "What colour will you paint the children's bedroom?",
+        answer: "B. I the same place.",
+        options: [
+            "A. Only for half an hour",
+            "B. I the same place.",
+            "C. It's the same.",
+            "D. same place.",
+
+        ]
+    },
+    {
+        namb: 4,
+        question: "What colour will t the children's bedroom?",
+        answer: "B. place.",
+        options: [
+            "A. Only for half an hour",
+            "B. place.",
+            "C. It's the same.",
+            "D. same place.",
+
+        ]
+    },
+    {
+        namb: 4,
+        question: "What colour will t the children's bedroom?",
+        answer: "B. place.",
+        options: [
+            "A. Only for half an hour",
+            "B. place.",
+            "C. It's the same.",
+            "D. same place.",
+
+        ]
+    },
+   
+]
